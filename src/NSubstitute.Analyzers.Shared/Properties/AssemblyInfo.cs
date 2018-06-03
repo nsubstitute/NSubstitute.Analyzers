af72928cbc373bbd9b47178a34cb20462056cf49
tpodolak <misiek767@gmail.com>
2018-06-03 23:24:06 +0200

    [GH-2] - missing assemblyinfos added

---
+++ src/NSubstitute.Analyzers.Shared/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("NSubstitute.Analyzers.CSharp")]
+[assembly: InternalsVisibleTo("NSubstitute.Analyzers.Tests.CSharp")]
+[assembly: InternalsVisibleTo("NSubstitute.Analyzers.VisualBasic")]
+[assembly: InternalsVisibleTo("NSubstitute.Analyzers.Tests.VisualBasic")]
