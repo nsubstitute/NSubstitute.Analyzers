3876efb301e75a56a60f6cc78c68f4c792d7b699
tpodolak <misiek767@gmail.com>
2020-10-11 17:31:21 +0200

    [GH-153] - simplifying ConstructorArgumentsForInterfaceCodeFixProvider with operation API and SyntaxGenerator

--- src/NSubstitute.Analyzers.Shared/CodeFixProviders/AbstractConstructorArgumentsForInterfaceCodeFixProvider.cs
+++ src/NSubstitute.Analyzers.Shared/CodeFixProviders/AbstractConstructorArgumentsForInterfaceCodeFixProvider.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Operations;
+using NSubstitute.Analyzers.Shared.Extensions;
 using Document = Microsoft.CodeAnalysis.Document;
 
 namespace NSubstitute.Analyzers.Shared.CodeFixProviders
@@ -20,32 +23,67 @@ namespace NSubstitute.Analyzers.Shared.CodeFixProviders
         public override Task RegisterCodeFixesAsync(CodeFixContext context)
         {
             var diagnostic = context.Diagnostics.FirstOrDefault(diag => diag.Descriptor.Id == DiagnosticIdentifiers.SubstituteConstructorArgumentsForInterface);
-            if (diagnostic != null)
+            if (diagnostic == null)
             {
-                var codeAction = CodeAction.Create("Remove constructor arguments", ct => CreateChangedDocument(ct, context, diagnostic), nameof(AbstractConstructorArgumentsForInterfaceCodeFixProvider<TInvocationExpressionSyntax>));
-                context.RegisterCodeFix(codeAction, diagnostic);
+                return Task.CompletedTask;
             }
+
+            var codeAction = CodeAction.Create(
+                "Remove constructor arguments",
+                ct => CreateChangedDocument(ct, context, diagnostic),
+                nameof(AbstractConstructorArgumentsForInterfaceCodeFixProvider<TInvocationExpressionSyntax>));
+
+            context.RegisterCodeFix(codeAction, diagnostic);
 
             return Task.CompletedTask;
         }
 
-        protected abstract TInvocationExpressionSyntax GetInvocationExpressionSyntaxWithEmptyArgumentList(TInvocationExpressionSyntax invocationExpressionSyntax);
-
-        protected abstract TInvocationExpressionSyntax GetInvocationExpressionSyntaxWithNullConstructorArgument(TInvocationExpressionSyntax invocationExpressionSyntax);
+        protected abstract SyntaxNode GetExpression(TInvocationExpressionSyntax invocationExpressionSyntax);
 
         private async Task<Document> CreateChangedDocument(CancellationToken cancellationToken, CodeFixContext context, Diagnostic diagnostic)
         {
+            var documentEditor = await DocumentEditor.CreateAsync(context.Document, cancellationToken);
+            var syntaxGenerator = documentEditor.Generator;
+
             var root = await context.Document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
 
             var invocation = (TInvocationExpressionSyntax)root.FindNode(diagnostic.Location.SourceSpan, getInnermostNodeForTie: true);
             var semanticModel = await context.Document.GetSemanticModelAsync(cancellationToken);
-            var updatedInvocation = semanticModel.GetSymbolInfo(invocation).Symbol is IMethodSymbol methodSymbol &&
-                                    methodSymbol.IsGenericMethod
-                ? GetInvocationExpressionSyntaxWithEmptyArgumentList(invocation)
-                : GetInvocationExpressionSyntaxWithNullConstructorArgument(invocation);
 
-            var replacedRoot = root.ReplaceNode(invocation, updatedInvocation);
-            return context.Document.WithSyntaxRoot(replacedRoot);
+            if (!(semanticModel.GetOperation(invocation) is IInvocationOperation invocationOperation))
+            {
+                return context.Document;
+            }
+
+            var expression = GetExpression(invocation);
+            var updatedInvocation = invocationOperation.TargetMethod.IsGenericMethod
+                ? GetInvocationExpressionSyntaxWithEmptyArgumentList(syntaxGenerator, expression)
+                : GetInvocationExpressionSyntaxWithNullConstructorArgument(syntaxGenerator, invocationOperation,  expression);
+
+            documentEditor.ReplaceNode(invocation, updatedInvocation);
+
+            return documentEditor.GetChangedDocument();
+        }
+
+        private static SyntaxNode GetInvocationExpressionSyntaxWithEmptyArgumentList(
+            SyntaxGenerator syntaxGenerator,
+            SyntaxNode expression)
+        {
+            return syntaxGenerator.InvocationExpression(expression);
+        }
+
+        private static SyntaxNode GetInvocationExpressionSyntaxWithNullConstructorArgument(
+            SyntaxGenerator generator,
+            IInvocationOperation invocationOperation,
+            SyntaxNode expression)
+        {
+            var nullArgument = generator.Argument(generator.NullLiteralExpression());
+            var newArguments = new[]
+            {
+                invocationOperation.GetOrderedArgumentOperationsWithoutInstanceArgument().First().Syntax, nullArgument
+            };
+
+            return generator.InvocationExpression(expression, newArguments);
         }
     }
 }
--- src/NSubstitute.Analyzers.VisualBasic/CodeFixProviders/ConstructorArgumentsForInterfaceCodeFixProvider.cs
+++ src/NSubstitute.Analyzers.VisualBasic/CodeFixProviders/ConstructorArgumentsForInterfaceCodeFixProvider.cs
@@ -1,27 +1,13 @@
-﻿using System.Linq;
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
-using Microsoft.CodeAnalysis.VisualBasic;
 using Microsoft.CodeAnalysis.VisualBasic.Syntax;
 using NSubstitute.Analyzers.Shared.CodeFixProviders;
-using static Microsoft.CodeAnalysis.VisualBasic.SyntaxFactory;
 
 namespace NSubstitute.Analyzers.VisualBasic.CodeFixProviders
 {
     [ExportCodeFixProvider(LanguageNames.VisualBasic)]
     internal sealed class ConstructorArgumentsForInterfaceCodeFixProvider : AbstractConstructorArgumentsForInterfaceCodeFixProvider<InvocationExpressionSyntax>
     {
-        protected override InvocationExpressionSyntax GetInvocationExpressionSyntaxWithEmptyArgumentList(InvocationExpressionSyntax invocationExpressionSyntax)
-        {
-            return invocationExpressionSyntax.WithArgumentList(ArgumentList());
-        }
-
-        protected override InvocationExpressionSyntax GetInvocationExpressionSyntaxWithNullConstructorArgument(InvocationExpressionSyntax invocationExpressionSyntax)
-        {
-            var nullSyntax = SimpleArgument(LiteralExpression(SyntaxKind.NothingLiteralExpression, Token(SyntaxKind.NothingKeyword)));
-            var seconArgument = invocationExpressionSyntax.ArgumentList.Arguments.Skip(1).First();
-            var argumentListSyntax = invocationExpressionSyntax.ArgumentList.ReplaceNode(seconArgument, nullSyntax);
-            return invocationExpressionSyntax.WithArgumentList(argumentListSyntax);
-        }
+        protected override SyntaxNode GetExpression(InvocationExpressionSyntax invocationExpressionSyntax) => invocationExpressionSyntax.Expression;
     }
 }
