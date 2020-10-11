94424c74c686811988b8d180e0e64fa83a47c1a2
tpodolak <misiek767@gmail.com>
2020-10-11 17:31:21 +0200

    [GH-xxx] - simplifying ConstructorArgumentsForInterfaceCodeFixProvider with operation API and SyntaxGenerator

--- src/NSubstitute.Analyzers.CSharp/CodeFixProviders/ConstructorArgumentsForInterfaceCodeFixProvider.cs
+++ src/NSubstitute.Analyzers.CSharp/CodeFixProviders/ConstructorArgumentsForInterfaceCodeFixProvider.cs
@@ -1,27 +1,13 @@
-﻿using System.Linq;
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using NSubstitute.Analyzers.Shared.CodeFixProviders;
-using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace NSubstitute.Analyzers.CSharp.CodeFixProviders
 {
     [ExportCodeFixProvider(LanguageNames.CSharp)]
     internal sealed class ConstructorArgumentsForInterfaceCodeFixProvider : AbstractConstructorArgumentsForInterfaceCodeFixProvider<InvocationExpressionSyntax>
     {
-        protected override InvocationExpressionSyntax GetInvocationExpressionSyntaxWithEmptyArgumentList(InvocationExpressionSyntax invocationExpressionSyntax)
-        {
-            return invocationExpressionSyntax.WithArgumentList(ArgumentList());
-        }
-
-        protected override InvocationExpressionSyntax GetInvocationExpressionSyntaxWithNullConstructorArgument(InvocationExpressionSyntax invocationExpressionSyntax)
-        {
-            var nullSyntax = Argument(LiteralExpression(SyntaxKind.NullLiteralExpression));
-            var seconArgument = invocationExpressionSyntax.ArgumentList.Arguments.Skip(1).First();
-            var argumentListSyntax = invocationExpressionSyntax.ArgumentList.ReplaceNode(seconArgument, nullSyntax);
-            return invocationExpressionSyntax.WithArgumentList(argumentListSyntax);
-        }
+        protected override SyntaxNode GetExpression(InvocationExpressionSyntax invocationExpressionSyntax) => invocationExpressionSyntax.Expression;
     }
 }
